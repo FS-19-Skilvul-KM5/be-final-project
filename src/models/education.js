12cf1ab5faf7bf1082a96d1e5397bdf024f74d12
muhammadaafiff <muhammad.afif250302@gmail.com>
2023-11-28 00:11:44 +0700

    bikin controller model route nya education

---
+++ src/models/education.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const EducationSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  video: { type: String, required: false },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Education = mongoose.model("Education", EducationSchema);
+
+module.exports = Education;
